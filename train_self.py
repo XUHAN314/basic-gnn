8be0cb25f0a0ff24093cd3d9c96f1ea4acf322a6
XUHAN314 <xuhanseu@hotmail.com>
2023-02-03 10:42:32 -0500

    initial

---
+++ train_self.py
@@ -0,0 +1,208 @@
+import torch
+import networkx as nx
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.nn import Linear
+from torch_geometric.datasets import KarateClub
+from torch_geometric.utils import to_dense_adj
+from torch_geometric.utils import to_networkx
+from torch_scatter import scatter
+
+def visualize_graph(G, color):
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
+                     node_color=color, cmap="Set2")
+    plt.show()
+
+
+
+
+
+dataset = KarateClub()
+
+print('Dataset properties')
+print('==============================================================')
+print(f'Dataset: {dataset}') #This prints the name of the dataset
+print(f'Number of graphs in the dataset: {len(dataset)}')
+print(f'Number of features: {dataset.num_features}') #Number of features each node in the dataset has
+print(f'Number of classes: {dataset.num_classes}') #Number of classes that a node can be classified into
+
+
+#Since we have one graph in the dataset, we will select the graph and explore it's properties
+
+data = dataset[0]
+print('Graph properties')
+print('==============================================================')
+
+# Gather some statistics about the graph.
+print(f'Number of nodes: {data.num_nodes}') #Number of nodes in the graph
+print(f'Number of edges: {data.num_edges}') #Number of edges in the graph
+print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}') # Average number of nodes in the graph
+print(f'Contains isolated nodes: {data.has_isolated_nodes()}') #Does the graph contains nodes that are not connected
+print(f'Contains self-loops: {data.has_self_loops()}') #Does the graph contains nodes that are linked to themselves
+print(f'Is undirected: {data.is_undirected()}') #Is the graph an undirected graph
+
+
+
+
+class GCNConv(nn.Module):
+
+    def __init__(self, c_in, c_out):
+        super().__init__()
+        self.projection = nn.Linear(c_in, c_out)
+
+    def forward(self, node_feats, adj_matrix):
+        """
+        Inputs:
+            node_feats - Tensor with node features of shape [batch_size, num_nodes, c_in]
+            adj_matrix - Batch of adjacency matrices of the graph. If there is an edge from i to j, adj_matrix[b,i,j]=1 else 0.
+                         Supports directed edges by non-symmetric matrices. Assumes to already have added the identity connections.
+                         Shape: [batch_size, num_nodes, num_nodes]
+        """
+        # Num neighbours = number of incoming edges
+        num_neighbours = adj_matrix.sum(dim=-1, keepdims=True)
+        node_feats = self.projection(node_feats)
+        node_feats = torch.bmm(adj_matrix, node_feats)
+        node_feats = node_feats / num_neighbours
+        return node_feats
+
+
+
+class SAGEConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        self.in_channels=in_channels
+        self.out_channels=out_channels
+        self.w1=nn.Linear(in_channels, out_channels, bias=False)
+        self.w2=nn.Linear(in_channels, out_channels, bias=False)
+        self.w3=nn.Linear(in_channels, out_channels)
+
+    def forward(self, node_features, edge_index):
+        receiver_features = scatter(self.w3(node_features),edge_index[1],dim=1)
+        receiver_features = self.w2(receiver_features)
+        node_features = self.w1(node_features)+receiver_features
+        return receiver_features
+
+class GATLayer(nn.Module):
+
+    def __init__(self, c_in, c_out, num_heads=1, concat_heads=True, alpha=0.2):
+        """
+        Inputs:
+            c_in - Dimensionality of input features
+            c_out - Dimensionality of output features
+            num_heads - Number of heads, i.e. attention mechanisms to apply in parallel. The
+                        output features are equally split up over the heads if concat_heads=True.
+            concat_heads - If True, the output of the different heads is concatenated instead of averaged.
+            alpha - Negative slope of the LeakyReLU activation.
+        """
+        super().__init__()
+        self.num_heads = num_heads
+        self.concat_heads = concat_heads
+        if self.concat_heads:
+            assert c_out % num_heads == 0, "Number of output features must be a multiple of the count of heads."
+            c_out = c_out // num_heads
+
+        # Sub-modules and parameters needed in the layer
+        self.projection = nn.Linear(c_in, c_out * num_heads)
+        self.a = nn.Parameter(torch.Tensor(num_heads, 2 * c_out)) # One per head
+        self.leakyrelu = nn.LeakyReLU(alpha)
+
+        # Initialization from the original implementation
+        nn.init.xavier_uniform_(self.projection.weight.data, gain=1.414)
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+
+    def forward(self, node_feats, adj_matrix, print_attn_probs=False):
+        """
+        Inputs:
+            node_feats - Input features of the node. Shape: [batch_size, c_in]
+            adj_matrix - Adjacency matrix including self-connections. Shape: [batch_size, num_nodes, num_nodes]
+            print_attn_probs - If True, the attention weights are printed during the forward pass (for debugging purposes)
+        """
+        batch_size, num_nodes = node_feats.size(0), node_feats.size(1)
+
+        # Apply linear layer and sort nodes by head
+        node_feats = self.projection(node_feats)
+        node_feats = node_feats.view(batch_size, num_nodes, self.num_heads, -1)
+
+        # We need to calculate the attention logits for every edge in the adjacency matrix
+        # Doing this on all possible combinations of nodes is very expensive
+        # => Create a tensor of [W*h_i||W*h_j] with i and j being the indices of all edges
+        edges = adj_matrix.nonzero(as_tuple=False) # Returns indices where the adjacency matrix is not 0 => edges
+        node_feats_flat = node_feats.view(batch_size * num_nodes, self.num_heads, -1)
+        edge_indices_row = edges[:,0] * num_nodes + edges[:,1]
+        edge_indices_col = edges[:,0] * num_nodes + edges[:,2]
+        a_input = torch.cat([
+            torch.index_select(input=node_feats_flat, index=edge_indices_row, dim=0),
+            torch.index_select(input=node_feats_flat, index=edge_indices_col, dim=0)
+        ], dim=-1) # Index select returns a tensor with node_feats_flat being indexed at the desired positions along dim=0
+
+        # Calculate attention MLP output (independent for each head)
+        attn_logits = torch.einsum('bhc,hc->bh', a_input, self.a)
+        attn_logits = self.leakyrelu(attn_logits)
+
+        # Map list of attention values back into a matrix
+        attn_matrix = attn_logits.new_zeros(adj_matrix.shape+(self.num_heads,)).fill_(-9e15)
+        attn_matrix[adj_matrix[...,None].repeat(1,1,1,self.num_heads) == 1] = attn_logits.reshape(-1)
+
+        # Weighted average of attention
+        attn_probs = F.softmax(attn_matrix, dim=2)
+        if print_attn_probs:
+            print("Attention probs\n", attn_probs.permute(0, 3, 1, 2))
+        node_feats = torch.einsum('bijh,bjhc->bihc', attn_probs, node_feats)
+
+        # If heads should be concatenated, we can do this by reshaping. Otherwise, take mean
+        if self.concat_heads:
+            node_feats = node_feats.reshape(batch_size, num_nodes, -1)
+        else:
+            node_feats = node_feats.mean(dim=2)
+
+        return node_feats
+
+
+
+
+
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super(GCN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GCNConv(dataset.num_features, 4)
+        self.conv2 = GCNConv(4, 4)
+        self.conv3 = GCNConv(4, 2)
+        self.classifier = Linear(2, dataset.num_classes)
+
+    def forward(self, x, adj_matrix):
+        h = self.conv1(x, adj_matrix)
+        h = h.tanh()
+        h = self.conv2(h, adj_matrix)
+        h = h.tanh()
+        h = self.conv3(h, adj_matrix)
+        h = h.tanh()  # Final GNN embedding space.
+
+        # Apply a final (linear) classifier.
+        out = self.classifier(h)
+
+        return out, h
+
+
+model = GCN()
+print(model)
+
+criterion = torch.nn.CrossEntropyLoss()  #Initialize the CrossEntropyLoss function.
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Initialize the Adam optimizer.
+
+def train(data):
+    optimizer.zero_grad()  # Clear gradients.
+    adj_matrix = to_dense_adj(data.edge_index).squeeze(0)
+    #x=data.x.unsqueeze(0)
+    out, h = model(data.x, adj_matrix)  # Perform a single forward pass.
+    loss = criterion(out.squeeze(0), data.y)  # Compute the loss solely based on the training nodes.
+    loss.backward()  # Derive gradients.
+    optimizer.step()  # Update parameters based on gradients.
+    return loss, h
+
+for epoch in range(401):
+    loss, h = train(data)
+    print(f'Epoch: {epoch}, Loss: {loss}')
